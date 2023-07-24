eeef77944459cf5579dc3d98638bdc126990f37a
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-25 02:47:09 +0530

    variables

--- src/test.py
+++ src/test.py
@@ -1,1 +1,18 @@
+# hellow world
+
 print('Hellow World');
+print('A'*5);
+
+# ---------variable declaration-----------
+
+# number
+number=10;
+
+# float number
+price=2.50;
+
+# String
+name="Pathum";
+
+# boolean
+is_Good=True;
