893297fcab3451eec3d7f33328efc33be69e2e53
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-30 21:26:37 +0530

    string methods

--- src/test.py
+++ src/test.py
@@ -20,15 +20,15 @@ is_Good=True;
 
 # -----------get user inputs------------
 
-myName=input('What is Your name ? ');
-print('hi '+myName);
+# myName=input('What is Your name ? ');
+# print('hi '+myName);
 
-# simple task
-my_Name=input('Your Name :');
-my_age=input('Your age :');
-age=2023-int(my_age);
+# # simple task
+# my_Name=input('Your Name :');
+# my_age=input('Your age :');
+# age=2023-int(my_age);
 
-print('Birth Year :'+age);
+# print('Birth Year :'+age);
 
 
 # ----------string in python------------
@@ -55,3 +55,22 @@ print(hellow[0:3]);
 
 print(hellow[:]);
 # output=python for me
+
+# -----------------------------------------
+
+# String methods
+
+country="Sri Lanka";
+
+# text convert upper and lower
+print(country.upper());
+print(country.lower());
+
+# return index
+print(country.find('L'));
+
+# replace word
+print(country.replace('Lanka','Matha'));
+
+# return boolean
+print('Sri' in country);
